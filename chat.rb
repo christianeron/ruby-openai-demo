6d22fe8cd4022e85f4f22664234af5f1ffc4c47c
christianeron <christianeron1@gmail.com>
2024-04-25 20:05:04 +0000

    challenge done

--- chat.rb
+++ chat.rb
@@ -2,26 +2,54 @@ require "openai"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
 
-# Prepare an Array of previous messages
-message_list = [
-  {
-    "role" => "system",
-    "content" => "You are a helpful assistant who talks like Shakespeare."
-  },
-  {
-    "role" => "user",
-    "content" => "Hello! What are the best spots for pizza in Chicago?"
-  }
-]
+api_content = ""
+api_message = ""
+user_response = ""
 
-# Call the API to get the next message from GPT
-api_response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: message_list
-  }
-)
+line = ""
+50.times do 
+  line = line + "-"
+end
 
-pp api_response
+puts "Hello! How can I help you today?"
+puts line
+user_response = gets.chomp
 
-list_of_messages.push(ai_response)
+while user_response != "bye"
+
+  # Prepare an Array of previous messages
+  
+  api_content = api_content + api_message + user_response
+  
+  message_list = [
+    {
+      "role" => "system",
+      "content" => "You are a helpful assistant."
+    },
+    {
+      "role" => "user",
+      "content" => "#{api_content}"
+    }
+  ]
+
+  # Call the API to get the next message from GPT
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: message_list
+    }
+  )
+
+  api_message = api_response.fetch("choices").at(0).fetch("message").fetch("content")
+
+  puts line
+
+  puts api_message
+
+  puts line
+
+  user_response = gets.chomp
+end
+
+puts line
+puts "Goodbye"
